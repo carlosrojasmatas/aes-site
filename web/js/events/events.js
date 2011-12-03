4d598cc0a5e34d900c8b5406e8ae644fc1711bc4
Carlos <carlos.rojasmatas@gmail.com>
2011-12-02 22:17:25 -0300

    Working on events

--- web/js/events/events.js
+++ web/js/events/events.js
@@ -26,7 +26,7 @@ $(function() {
 		onDayLinkClick: function(dateIn) { 
         },
         onMonthChanging: function(dateIn) {
-        	alert(dateIn);
+        	alert(dateIn.getMonth());
 			//this could be an Ajax call to the backend to get this months events
 			//var events = [ 	{ "EventID": 7, "StartDate": new Date(2012, 1, 1), "Title": "10:00 pm - EventTitle1", "URL": "#", "Description": "This is a sample event description", "CssClass": "Birthday" },
 			//				{ "EventID": 8, "StartDate": new Date(2012, 1, 2), "Title": "9:30 pm - this is a much longer title", "URL": "#", "Description": "This //is a sample event description", "CssClass": "Meeting" }
