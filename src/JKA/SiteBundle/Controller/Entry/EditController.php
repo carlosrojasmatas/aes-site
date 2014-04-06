eb7c0d0839b053895ebe12544c5b1b3ec005fcb6
cmatas <cmatas@despegar.com>
2014-04-06 13:20:13 -0300

    delete files

--- src/JKA/SiteBundle/Controller/Entry/EditController.php
+++ src/JKA/SiteBundle/Controller/Entry/EditController.php
@@ -4,6 +4,7 @@ namespace JKA\SiteBundle\Controller\Entry;
 
 use Admingenerated\JKASiteBundle\BaseEntryController\EditController as BaseEditController;
 use JKA\SiteBundle\Service\FileService\FileService;
+use JKA\SiteBundle\Entity\Entry;
 
 /**
  * EditController
@@ -11,7 +12,7 @@ use JKA\SiteBundle\Service\FileService\FileService;
 class EditController extends BaseEditController
 {
 	
-	public function preSave(\Symfony\Component\Form\Form $form, \JKA\SiteBundle\Entity\Entry $entry){
+	public function preSave(\Symfony\Component\Form\Form $form, Entry $entry){
 		$fs = $this->get("file.service");
 
 		if(Entry::$EVENT != $entry->getType()) {
@@ -21,7 +22,7 @@ class EditController extends BaseEditController
 		}
 	}
 	
-    protected function getAdditionalRenderParameters(\JKA\SiteBundle\Entity\Entry $Entry)
+    protected function getAdditionalRenderParameters(Entry $Entry)
     {
         return array("image_path" => $Entry->getPath());
     }
